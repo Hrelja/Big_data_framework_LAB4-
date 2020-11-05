38cfac1a7b703ea72f4f007e3cbc9a16a6dfeebc
edin hrelja <edinhrelja@macbook-pro-de-edin.home>
2020-11-05 13:20:07 +0100

    first commit'

---
+++ src/main/java/com/opstty/AppDriver.java
@@ -0,0 +1,26 @@
+package com.opstty;
+
+import com.opstty.job.WordCount;
+import com.opstty.job.ArrandisementCount;
+import org.apache.hadoop.util.ProgramDriver;
+
+public class AppDriver {
+    public static void main(String argv[]) {
+        int exitCode = -1;
+        ProgramDriver programDriver = new ProgramDriver();
+
+        try {
+            programDriver.addClass("wordcount", WordCount.class,
+                    "A map/reduce program that counts the words in the input files.");
+
+            programDriver.addClass("arrondissement_count", ArrandisementCount.class,
+                            "A map/reduce program that give the number of arrondissement.");
+
+            exitCode = programDriver.run(argv);
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        System.exit(exitCode);
+    }
+}
