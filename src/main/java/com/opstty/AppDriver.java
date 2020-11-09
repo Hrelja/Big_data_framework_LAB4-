52a0bd7fc036c0208afa4bce8f31661bdf53a335
Men0x <38214356+Men0x@users.noreply.github.com>
2020-11-09 20:37:47 +0100

    gregoire commit 2

--- src/main/java/com/opstty/AppDriver.java
+++ src/main/java/com/opstty/AppDriver.java
@@ -1,9 +1,6 @@
 package com.opstty;
 
-import com.opstty.job.EachSpeciesAverage;
-import com.opstty.job.SpeciesCount;
-import com.opstty.job.WordCount;
-import com.opstty.job.ArrandisementCount;
+import com.opstty.job.*;
 import org.apache.hadoop.util.ProgramDriver;
 
 public class AppDriver {
@@ -23,6 +20,8 @@ public class AppDriver {
                     "A map/reduce program that give the number of trees for each species.");
             programDriver.addClass("countTallTree", EachSpeciesAverage.class,
                     "A map/reduce program that give the tallest tree for each species.");
+            programDriver.addClass("SmallestToLargest", SmallestToLargest.class,
+                    "A map/reduce program that give the smallest to largest tree for all species.");
             exitCode = programDriver.run(argv);
         } catch (Throwable throwable) {
             throwable.printStackTrace();
